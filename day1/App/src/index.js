8a4456b311c3bc518f4dcb1411fb131fee4a5354
Gary <Gary19911008@outlook.com>
2017-11-15 18:22:47 +0800

    11.15.2017 react update

---
+++ day1/App/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+//直接导入的是 export default
+// {} 导入的是 export 导出的东西
+import sayHello, {name, age} from "./moduleTest.js";
+
+import registerServiceWorker from './registerServiceWorker';
+
+console.log(sayHello);
+console.log(name);
+console.log(age);
+
+/*
+
+import xxx from yyyy  导入模块， 类似于nodejs中 require
+
+export  类似于 nodejs中的 module.exports
+*/
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
